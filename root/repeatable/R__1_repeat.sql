d994dfba8170d3e8d124727d57a2601b0dbc4fb8
gaurav-kipi <gaurav.r.bhatt@kipi.bi>
2023-01-23 19:32:41 +0530

    test2

--- root/repeatable/R__1_repeat.sql
+++ root/repeatable/R__1_repeat.sql
@@ -5,11 +5,7 @@ returns varchar
 language sql
 as 
 $$
-declare
-    var_one int;
-begin
-    var_one := 3;
-    returns var_one;
+    returns "var_one";
 end;
 $$
 ;
--- root/version/V1.1.2_second_file.sql
+++
@@ -1,3 +0,0 @@
-use schema demo_schema;
-
-alter table if exists demo_table add column new_col number[;]
