75cf012c208f37cbe758ffb9f323ad818542ea18
gaurav-kipi <gaurav.r.bhatt@kipi.bi>
2023-01-23 19:28:05 +0530

    commit run

--- root/V1.1.1__first_file.sql
+++ root/V1.1.1__first_file.sql
@@ -1,5 +1,5 @@
 create schema if not exists demo_schema;
-use demo_schema;
+use schema demo_schema;
 
 create table if not exists demo_table(
     num int,
--- root/version/V1.1.2_second_file.sql
+++ root/version/V1.1.2_second_file.sql
@@ -1,3 +1,3 @@
-use demo_schema;
+use schema demo_schema;
 
 alter table if exists demo_table add column new_col number[;]
